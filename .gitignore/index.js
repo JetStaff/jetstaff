7c5815cfe8429f3ba2cbc48ac87fea9947b3834c
JetStaff <39329647+JetStaff@users.noreply.github.com>
2018-05-16 09:53:36 +0200

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js')
+const bot = new Discord.Client()
+
+bot.login('NDQ1NTc3NzM5OTI4NjAwNTc2.DdsgJQ.hugw2ocERhxXqei1c7g4R-Xfj6c')
+
+bot.on('message', function (message) { 
+if (message.content === 'Bonjour') {
+    message.reply ('Hey comment vas tu ?')
+}
+} )
+
+bot.on ('message' ,function (message) {
+    if (message.content === 'salut') {
+        message.channel.send('Hey mon khey')
+    }
+})
+
+bot.on('ready', function() {
+    bot.user.setGame('JetBot, '/help);
+    console.log('Le Bot est désormais connecté');
+});
+
